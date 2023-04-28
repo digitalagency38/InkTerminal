b3322f3f2c5356847f1bdeea682506358fbafca4
Гончаров Сергей <Goncharov-5769@main-fer.fnpz.uz>
2023-04-28 18:04:05 +0500

    Доработки

--- src/blocks/modules/main_slider/main_slider.js
+++ src/blocks/modules/main_slider/main_slider.js
@@ -1,13 +1,23 @@
-const Header = class Header {
-    constructor({someVareible}){
-        this.someVareible = someVareible;
+import Glide from '@glidejs/glide';
+
+const MainSlider = class MainSlider {
+    constructor() {
+        this.slider = null;
+        this.index = 0;
     }
-    someMethod() {
-        console.log(this.someVareible);
+    initSlider() {
+        if (!document.querySelector('.main_slider.glide')) return;
+
+        this.slider = new Glide('.main_slider.glide', {
+            perView: 1,
+            swipeThreshold: false,
+            dragThreshold: false,
+            type: 'carousel'
+        }).mount()
     }
     init() {
-        this.someMethod();
+        this.initSlider();
     }
 }
 
-export default Header;
+export default MainSlider;
--- src/js/app.js
+++ src/js/app.js
@@ -4,34 +4,14 @@ globalFunctions.isWebp();
 import Vue from 'vue/dist/vue.js';
 import $ from 'jquery';
 
-import Header from '../blocks/modules/main_slider/main_slider.js';
+import MainSlider from '../blocks/modules/main_slider/main_slider.js';
 
 window.app = new Vue({
     el: '#app',
     data: () => ({
-        sizes: {
-            tablet: 1024,
-            mobile: 768,
-            window: window.innerWidth
-        },
-        header: new Header({
-            someVareible: 'someVareible'
-        })
+        mainslider: new MainSlider()
     }),
-    beforeCreate() {        
-        window.addEventListener('resize', () => {
-            this.sizes.window = window.innerWidth;
-        });
-    },
-    beforeMount() {
-        this.header.init();
-    },
-    computed: {
-        isMobile: function () {
-            return this.sizes.window < this.sizes.mobile;
-        },
-        isTablet: function () {
-            return this.sizes.window < this.sizes.tablet && this.sizes.window > this.sizes.mobile;
-        }
-    },
+    mounted() {
+        this.mainslider.init();
+    }
 });
