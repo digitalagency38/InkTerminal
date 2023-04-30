295386ab12ee92ea0eb4d68e2474d406f3cd985a
Sergey Goncharovр <doimpos@gmail.com>
2023-04-30 15:04:39 +0500

    block done

---
+++ src/blocks/modules/block_anketa/main_slider.js
@@ -0,0 +1,21 @@
+const MainSlider = class MainSlider {
+    constructor() {
+        // this.slider = null;
+        // this.index = 0;
+    }
+    initSlider() {
+        // if (!document.querySelector('.main_slider.glide')) return;
+
+        // this.slider = new Glide('.main_slider.glide', {
+        //     perView: 1,
+        //     swipeThreshold: false,
+        //     dragThreshold: false,
+        //     type: 'carousel'
+        // }).mount()
+    }
+    init() {
+        this.initSlider();
+    }
+}
+
+export default MainSlider;
